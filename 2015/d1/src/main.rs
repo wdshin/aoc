c3ca9f8fa81bbafd3bd5edb3847811400aeeb484
ElliotShin <wodshin@gmail.com>
2022-12-02 00:02:47 +0900

    2015 d1,d2

---
+++ 2015/d1/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input.txt")?;
+    let mut floor = 0;
+    for c in input.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        }
+    }
+    println!("{}", floor);
+    Ok(())
+}
